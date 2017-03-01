74fe4aa4ac85db486d5acb0dfca2893e84ff0525
genes <lizzysollars@hotmail.co.uk>
2017-03-01 17:44:30 +0000

    script now makes excel file of gaps and annoates with coverage info

--- gapsInCoverage.py
+++ gapsInCoverage.py
@@ -3,6 +3,7 @@
 import os
 import glob
 import json
+import numpy
 import traceback
 import pysam
 import subprocess
@@ -58,16 +59,19 @@ class gapsInCoverage(IonPlugin):
                     new_bed.write(line)
         return new_bed
 
-    def runsambamba(self, results_dir, bedfile, barcode, bam, min_cov, max_cov, prefix):
+    def runsambamba(self, results_dir, bedfile, barcode, bam, max_cov=None, prefix=None):
         print "Running Sambamba " + barcode
-        outputfile = os.path.join(results_dir, (barcode + prefix + '_depth_base.txt'))
-        self.run_command('/home/ionadmin/sambamba_v0.6.5 depth base -o ' + outputfile + ' -c 0 -q 0 -L ' + bedfile + ' ' + bam)
-
-        outputfile_filtered = os.path.join(results_dir, (barcode + prefix + '_depth_base_filtered.txt'))
-        self.run_command('/home/ionadmin/sambamba_v0.6.5 depth base -o ' + outputfile_filtered + ' -c ' + str(min_cov) + ' -C ' + str(max_cov) + ' -q 0 -L ' + bedfile + ' ' + bam)
-
-        num_lines = sum(1 for line in open(outputfile_filtered))
-        return num_lines - 1
+        if prefix:
+            outputfile = os.path.join(results_dir, (barcode + prefix + '_depth_base.txt'))
+            self.run_command('/home/ionadmin/sambamba_v0.6.5 depth base -o ' + outputfile + ' -c 0 -q 0 -L ' + bedfile + ' ' + bam)
+            outputfile_filtered = os.path.join(results_dir, (barcode + prefix + '_depth_base_filtered.txt'))
+            self.run_command('/home/ionadmin/sambamba_v0.6.5 depth base -o ' + outputfile_filtered + ' -c 0 -C ' + str(max_cov) + ' -q 0 -L ' + bedfile + ' ' + bam)
+            num_lines = sum(1 for line in open(outputfile_filtered))
+            return num_lines - 1
+        else:
+            outputfile = os.path.join(results_dir, (barcode + '_depth_base.txt'))
+            self.run_command('/home/ionadmin/sambamba_v0.6.5 depth base -o ' + outputfile + ' -c 0 -q 0 -L ' + bedfile + ' ' + bam)
+            return outputfile
 
     def concat_files(self, file_one, file_two, results_dir, barcode, prefix):
         output_file = os.path.join(results_dir, (barcode + prefix + '.bed'))
@@ -82,12 +86,56 @@ class gapsInCoverage(IonPlugin):
             print('Error executing command: ' + str(e.returncode))
             exit(1)
 
+    def get_coverage(self, results_dir, barcode, prefix):
+        self.run_command('sed 1d ' + os.path.join(results_dir, (barcode + '_' + prefix + '_depth_base.txt')) + ' | sort -n -k 3,3 | cut -f3 | head -1 > ' + os.path.join(results_dir, barcode + '_min' + prefix + 'Coverage.txt'))
+        with open (os.path.join(results_dir, barcode + '_min' + prefix + 'Coverage.txt')) as min_cov_file:
+            min_cov = min_cov_file.readline().rstrip()
+        min_cov_file.close()
+        return min_cov
+
+    def write_to_excel(self, outfile, alamut_file, cov_file, num_gaps, min_cov):
+        outputfile = open(outfile, mode='w')
+        if num_gaps > 0:
+            with open (alamut_file) as gapsfile:
+                for line in gapsfile:
+                    line = line.rstrip()
+                    if 'chromosome' in line:
+                        outputfile.write(line+'\tbase_depth\n')
+                    else:
+                        chromosome, bp_pos, region, depth = line.split('\t')
+                        with open(cov_file) as covfile:
+                            for lin in covfile:
+                                if 'REF' in lin:
+                                    continue
+                                else:
+                                    array = lin.split('\t')
+                                    if (chromosome in array[0]) and (int(array[1]) == int(bp_pos) - 1):
+                                        a_cov = array[3]
+                                        c_cov = array[4]
+                                        g_cov = array[5]
+                                        t_cov = array[6]
+                                        del_cov = array[7]
+                                        ref_skip = array[8]
+                                        cov_string = chromosome+'\t'+bp_pos+'\t'+region+'\t'+depth+'\tA='+a_cov+';T='+t_cov+';G='+g_cov+';C='+c_cov+';del='+del_cov+';refskip='+ref_skip
+                                        outputfile.write(cov_string + '\n')
+                        covfile.close()
+            gapsfile.close()
+        else:
+            outputfile.write('No gaps found\nMinimum coverage = '+min_cov)
+        logfile.close()
+
+        outputfile.close()
+        return outfile
+
     def launch(self, data=None):
         self.log.info('Launching Plugin')
         data = self.load_startpluginjson()
         #print json.dumps(data, indent=4)
         info = {}
         results_dir = data['runinfo']['plugin']['results_dir']
+        run_name = data['expmeta']['results_name']
+        if not os.path.isdir('/results/for_review/' + run_name):
+            os.mkdir('/results/for_review/' + run_name)
         print "Results directory: "
         print results_dir + '\n'
         block_file = os.path.join(results_dir,'gapsInCoverage_block.html')
@@ -123,24 +171,33 @@ class gapsInCoverage(IonPlugin):
             info[barcode]["bam"] = bam
             bed_name = os.path.basename(info[barcode]["bed"]).replace(".bed", "")
 
-            exonic_gaps = self.runsambamba(results_dir, os.path.join(results_dir, (bed_name+'_exonic_noheader.bed')), barcode, bam, 0, 49, '_exonic')
-            intronic_gaps = self.runsambamba(results_dir, os.path.join(results_dir, (bed_name+'_intronic_noheader.bed')), barcode, bam, 0, 29, '_intronic')
+            depth_base_file = self.runsambamba(results_dir, os.path.join(results_dir, (bed_name+'_noheader.bed')), barcode, bam)
+            exonic_gaps = self.runsambamba(results_dir, os.path.join(results_dir, (bed_name+'_exonic_noheader.bed')), barcode, bam, 49, '_exonic')
+            intronic_gaps = self.runsambamba(results_dir, os.path.join(results_dir, (bed_name+'_intronic_noheader.bed')), barcode, bam, 29, '_intronic')
             total_gaps = int(exonic_gaps + intronic_gaps)
             print "Gaps in exons: " + str(exonic_gaps)
             print "Gaps in introns: " + str(intronic_gaps)
             print "Total gaps: " + str(total_gaps)
 
-            ##generate all_gaps.txt
+            ##generate gaps files
 
             gaps_bed_file = self.concat_files(os.path.join(results_dir, (barcode + '_exonic_depth_base_filtered.txt')), os.path.join(results_dir, (barcode + '_intronic_depth_base_filtered.txt')), results_dir, barcode, '_gaps')
-            depth_base_file = self.concat_files(os.path.join(results_dir, (barcode + '_exonic_depth_base.txt')), os.path.join(results_dir, (barcode + '_intronic_depth_base.txt')), results_dir, barcode, '_depth_base')
             gaps_in_sequencing = os.path.join(results_dir, barcode + '_gaps_in_sequencing.txt')
             alamut_file = os.path.join(results_dir, barcode + '_gaps_in_sequencing_alamut.txt')
 
             self.run_command(('/home/ionadmin/bedtools2/bin/intersectBed -wb -a ' + os.path.join(results_dir, (bed_name+'_noheader.bed')) + ' -b ' + gaps_bed_file + ' | cut -f1,2,4,12 | awk \'BEGIN{print "chromosome\tbp_pos\tregion\tdepth"}1\' > ' + gaps_in_sequencing))
             self.run_command(('awk \'NR==1 {print $0} NR>1 {print ($1"\t"$2+1"\t"$3"\t"$4)}\' ' + gaps_in_sequencing + ' > ' + alamut_file))
 
-            gaps_info = {'barcode': barcode, 'sample': info[barcode]["sample"], 'no_gaps': total_gaps, 'depth_per_base': depth_base_file, 'gaps_file': gaps_in_sequencing, 'alamut_gaps_file': alamut_file}
+            min_exon_cov = self.get_coverage(results_dir, barcode, 'exonic')
+            min_intron_cov = self.get_coverage(results_dir, barcode, 'intronic')
+            if min_intron_cov<= min_exon_cov:
+                min_cov = min_intron_cov
+            else:
+                min_cov = min_exon_cov
+
+            excel_file = self.write_to_excel(os.path.join('/results/for_review', run_name, barcode + '_' + info[barcode]['sample'] + '.xls'), alamut_file, depth_base_file, total_gaps, min_cov)
+
+            gaps_info = {'barcode': barcode, 'sample': info[barcode]["sample"], 'no_gaps': total_gaps, 'min_exon_cov': min_exon_cov, 'min_intron_cov': min_intron_cov, 'depth_per_base': depth_base_file, 'gaps_file': gaps_in_sequencing, 'alamut_gaps_file': alamut_file, 'excel_file': excel_file}
             gaps_result.append(gaps_info)
 
         #print json.dumps(info, indent=4)
