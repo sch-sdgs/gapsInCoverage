b3157d379f5612c137b01225817f08f83d232577
genes <lizzysollars@hotmail.co.uk>
2017-05-03 15:01:27 +0100

    getting ready for release

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /results/plugins/gapsInCoverage
+
+virtualenv gapsInCoverage_venv
+
+source /results/plugins/gapsInCoverage/gapsInCoverage_venv/bin/activate
+
+pip install -r requirements.txt
+
+
+
+
